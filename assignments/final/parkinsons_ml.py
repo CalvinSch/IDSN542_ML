f3f7014f5d8c4c11ab8ba29222bb1314b1a56625
Calvin <calvinschaul@gmail.com>
2025-11-20 00:34:09 -0500

    correlation step done

--- assignments/final/parkinsons_ml.py
+++ assignments/final/parkinsons_ml.py
@@ -2,12 +2,15 @@
 Calvin Schaul
 IDSN 542, Fall 2025
 cschaul@usc.edu
-Final Project Part 1
+Final Project Part 1 + 2
 '''
 
 from pathlib import Path
 import pandas as pd
 import sys
+
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
 
 
 def load_data(path):
@@ -56,6 +59,18 @@ def calculate_correlation_of_features(data, target_column):
 	target_corr = corr_matrix[target_column].sort_values(ascending=False)
 	print(f"\nCorrelation of features with '{target_column}':")
 	print(target_corr)
+	return target_corr
+	
+def plot_correlation_comparisons(data, attributes, correlation_values, threshold):
+    # Visualize correlations with a scatter matrix, focusing on the most correlated attributes (> 0.1)
+    correlated_attributes = [attr for attr in attributes if abs(correlation_values[attr]) > threshold]
+    print("\nAttributes with correlation above threshold", threshold, ":", correlated_attributes)
+    n_attrs = len(correlated_attributes)
+    # Set figure size: width and height proportional to number of attributes
+    figsize = (2.5 * n_attrs, 2.5 * n_attrs)
+    scatter_matrix(data[correlated_attributes], figsize=figsize)
+    plt.show() # You have to close the plot window to continue the program
+    pass
 
 
 def main():
@@ -69,11 +84,22 @@ def main():
 	print('\nFirst 3 rows:')
 	print(data.head(3))
 
-    # Check for missing data
+	# Check for missing data
 	check_missing_data(data)
 	
-    # Check correlation of features with diagnosis - Drop Dr ID because it's redacted anyways
-	calculate_correlation_of_features(data=data.drop(columns=["DoctorInCharge"]), target_column='Diagnosis')
+    # Drop irrelevant columns
+	data = data.drop(columns=["PatientID", "DoctorInCharge"])
+	
+	# Check correlation of features with diagnosis - Drop Dr ID because it's redacted anyways
+	corr_data = calculate_correlation_of_features(data, target_column='Diagnosis')
 
+	# Create lists of numerical attributes and categorical attributes
+	categorical_attributes = [col for col in data.columns if set(data[col]).issubset({0, 1, 2, 3})] # Categorical data encoded as 0,1,2,3 (yes, no, or level of education)
+	numerical_attributes = [col for col in data.columns if col not in categorical_attributes] # Rest are numerical attributes
+	print("\nCategorical attributes:", categorical_attributes)
+	print("\nNumerical attributes:", numerical_attributes)
+	
+	# Plotting numerical attributes for correlation comparison - control how correlated value should be in order to make it on to plot
+	plot_correlation_comparisons(data, numerical_attributes, corr_data, threshold=0.1)
 main()
 
